8f61860be1a7991dc296588844a9d315f360b236
Alexander Biderman <a_lightin@mail.ru>
2021-12-02 11:36:48 +0300

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,45 @@
+import java.util.*
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(inputFilename: String): Int {
+        var previous: Int? = null
+        var result = 0
+        getInputFile(inputFilename).forEachLine {
+            val current: Int = it.toInt()
+            if (previous != null && current > previous!!) result++
+            previous = current
+        }
+
+        return result
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(inputFilename: String): Int {
+        var previousSum = 0
+        val previousData: LinkedList<Int> = LinkedList()
+
+        var result = 0
+        getInputFile(inputFilename).forEachLine {
+            val current: Int = it.toInt()
+            if (previousData.size < 3) {
+                previousSum += current
+            }
+            else {
+                val newSum = previousSum - previousData[0] + current
+                if (newSum > previousSum)
+                    result++
+                previousData.removeFirst()
+                previousSum = newSum
+            }
+            previousData.add(current)
+        }
+
+        return result
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1("Day01_test") == 7)
+    check(part2("Day01_test") == 5)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(part1("Day01"))
+    println(part2("Day01"))
 }
